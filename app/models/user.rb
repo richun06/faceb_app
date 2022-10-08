c2586dd5d19e20238f0e133aed0321166c5f12ec
rika kaminoyama <richun.06@gmail.com>
2022-10-08 22:38:45 +0900

    文章投稿機能を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :blogs
+
   before_validation { email.downcase! }
 
   validates :name, presence: true, length: { maximum: 30 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :blogs, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+    collection do
+      post :confirm
+    end
+  end
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
 end
